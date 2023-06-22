74114e8ac57c30fada53068e7e90ac64586657db
pousoupou <stelios.papaspiros@gmail.com>
2023-06-22 20:39:39 +0300

    added a "test" to main()
    "test" the Cube class

--- MainGame.java
+++ MainGame.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class MainGame {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        ArrayList<Cube> list = new ArrayList<Cube>();
+
+        int K = 0;
+
+        System.out.print("ENTER K: ");
+        K = input.nextInt();
+
+        // Initialize a list with K cubes on the table (ordered)
+        for(int i = 1; i <= 3 * K; i++){
+            Cube cube = new Cube(i, i, 1);
+            list.add(i - 1, cube);
+        }
     }
 }
