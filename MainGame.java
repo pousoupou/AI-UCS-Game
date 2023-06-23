0cd77954e66403c11b5bc6abcd26f1728b28d101
pousoupou <stelios.papaspiros@gmail.com>
2023-06-23 15:14:25 +0300

    Update MainGame.java
    updated with tests

--- MainGame.java
+++ MainGame.java
@@ -3,18 +3,26 @@ import java.util.*;
 public class MainGame {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         ArrayList<Cube> list = new ArrayList<Cube>();
+
+        Table table = new Table();
 
         int K = 0;
 
         System.out.print("ENTER K: ");
         K = input.nextInt();
+        System.out.println();
 
         //TODO: Randomize the list
         // Initialize a list with K cubes on the table (ordered)
         for(int i = 1; i <= 3 * K; i++){
             Cube cube = new Cube(i, i, 1);
-            list.add(i - 1, cube);
+            list.add(cube);
+
+            table.addCube(cube);
         }
+
+        table.printTable();
     }
 }
