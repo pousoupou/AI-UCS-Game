04c06519175d702d4ed5c6c52c1aa3eba9319ff8
pousoupou <stelios.papaspiros@gmail.com>
2023-06-23 15:39:10 +0300

    init Node.java

--- Node.java
+++ Node.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
 public class Node {
+    private Table state;
+    private Double pathCost;
+    private Node parent;
+
+    public Node(Table state, Double pathCost, Node parent){
+        this.state = state;
+        this.pathCost = pathCost;
+        this.parent = parent;
+    }
+
     
 }
