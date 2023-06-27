e146604250f160d6f676ee21db6156236992f9d9
pousoupou <stelios.papaspiros@gmail.com>
2023-06-27 12:31:57 +0300

    Update Table.java
    just a bunch of tries to add all the moves

--- Table.java
+++ Table.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class Table {
+    private ArrayList<Table> moves;
     private ArrayList<Cube> list;
     private int K;
 
     public Table(int K){
         this.K = K;
-        list = new ArrayList<Cube>();
+        this.moves = new ArrayList<Table>();
+        this.list = new ArrayList<Cube>();
     }
 
     public int getK(){
@@ -22,7 +24,7 @@ public class Table {
     }
 
     public void addCube(Cube cube){
-        list.add(cube);
+        this.list.add(cube);
     }
 
     public Integer getByID(int ID){
@@ -35,42 +37,41 @@ public class Table {
     }
 
     public ArrayList<Table> generateMoves(){
-        ArrayList<Table> moves = new ArrayList<Table>();
-        ArrayList<Cube> currentBoard = this.getCubes();
+        ArrayList<Cube> currentTable = this.getCubes();
+        ArrayList<Cube> copyOfTable = new ArrayList<Cube>(currentTable);
 
         int X = 1;
         int Y = 1;
         int K = this.getK();
-        int index = 0;
 
-        for(Cube cube : currentBoard){
+        for(Cube cube : currentTable){
             int backupX = cube.getPosX();
             int backupY = cube.getPosY();
 
             for(Y = 1; Y <= 3; Y++){
                 for(X = 1; X <= 4*K; X++){
-                    if(Y > 1 && X > K){
-                        continue;
-                    }
-                    if(cube.isFree(currentBoard)){
-                        cube.setPosX(X);
-                        cube.setPosY(Y);
-    
-                        moves.get(index).addCube(cube);
-                        index++;
-                    }else if(!cube.isValid(currentBoard)){
+                    cube.setPosX(X);
+                    cube.setPosY(Y);
+                    if(cube.isValid(copyOfTable) && cube.isFree(copyOfTable)){
+                        Table table = new Table(K);
+                        table.setCubes(currentTable);
+                        
+                        this.moves.add(table);
+                    }else{
                         cube.setPosX(backupX);
                         cube.setPosY(backupY);
                         continue;
                     }
                 }
             }
+            cube.setPosX(backupX);
+            cube.setPosY(backupY);
         }
-        return moves;
+        return this.moves;
     }
 
     public Boolean isGoalState(){
-        int K = list.size()/3;
+        int K = this.getCubes().size()/3;
         int i = 0;
         for(Cube cube : list){
             if(cube.getPosX() == ((cube.getID() - 1) % K + 1) && cube.getPosY() == ((cube.getID() - 1) / K + 1)){
@@ -84,28 +85,35 @@ public class Table {
     }
 
     public void printTable(){
-        int  i = 0;
+        int i = 0;
         int J = 0;
         int K = 0;
 
-        while(i < list.size()){
-            if(list.get(i).getPosY() == 3){
-                System.out.print(list.get(i).getID() + " ");
-                J++;
-                if(J == 3){
-                    System.out.println();
-                }
-            }else if(list.get(i).getPosY() == 2){
-                System.out.print(list.get(i).getID() + " ");
-                K++;
-                if(K == 3){
-                    System.out.println();
-                }
-            }else{
-                System.out.print(list.get(i).getID() + " ");
-            }
-            i++;
+        // System.out.println(this.getCubes().get(0).getPosX());
+
+        for(Cube cube : this.getCubes()){
+            System.out.println("The cube with ID: " + cube.getID() + " is in X: " + cube.getPosX() + " Y: " + cube.getPosY());
         }
+
+        // ArrayList<Cube> list = this.getCubes();
+        // while(i < list.size()){
+        //     if(list.get(i).getPosY() == 3){
+        //         System.out.print(list.get(i).getID() + " ");
+        //         J++;
+        //         if(J == 3){
+        //             System.out.println();
+        //         }
+        //     }else if(list.get(i).getPosY() == 2){
+        //         System.out.print(list.get(i).getID() + " ");
+        //         K++;
+        //         if(K == 3){
+        //             System.out.println();
+        //         }
+        //     }else{
+        //         System.out.print(list.get(i).getID() + " ");
+        //     }
+        //     i++;
+        // }
         System.out.println();
     }
 }
