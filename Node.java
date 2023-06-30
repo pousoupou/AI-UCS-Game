00e393590dcdf214216e2eac6b366703bce8e912
Stylianos Papaspyros <stelios.papaspiros@gmail.com>
2023-06-30 18:06:57 +0300

    added setters/getters

--- Node.java
+++ Node.java
@@ -11,9 +11,32 @@ public class Node {
         this.parent = parent;
     }
 
-    //TODO: getters/setters
+    public Table getState(){
+        return this.state;
+    }
+
+    public Double getCost(){
+        return this.pathCost;
+    }
+
+    public Node getParent(){
+        return this.parent;
+    }
+
+    public void setState(Table state){
+        this.state = state;
+    }
+
+    public void setCost(Double pathCost){
+        this.pathCost = pathCost;
+    }
+
+    public void setParent(Node parent){
+        this.parent = parent;
+    }
 
     //TODO: gerateChildren()
+    
 
     //TODO: calculateCost()
 
